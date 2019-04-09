7b9515da0b7e4f8028ff0677143804f393ba0a4e
Peter Hsieh <chunhung.hsieh@gmail.com>
2019-04-09 16:00:47 +0900

    add end to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
 
-
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
@@ -11,6 +10,7 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
