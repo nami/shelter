138a3480bdfb50655a7e7a24af2a19ecde9fbf5c
Peter Hsieh <chunhung.hsieh@gmail.com>
2019-04-09 13:00:37 +0900

    pundit gem and policies added

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,35 @@
+class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    return true
+  end
+
+  def show?
+    return true
+  end
+
+  def new?
+    return true
+  end
+
+  def create?
+    return true
+  end
+
+  def edit?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
