13dfaa3f584efaf7b069b5a8ed3085905a54360f
nami izarra <nami.izarra@gmail.com>
2019-04-09 13:21:32 +0900

    in users table default value of verified is false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_134713) do
+ActiveRecord::Schema.define(version: 2019_04_09_041946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2019_04_08_134713) do
     t.string "role"
     t.string "phone"
     t.string "address"
-    t.boolean "verified"
+    t.boolean "verified", default: false
     t.string "photo_id"
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
