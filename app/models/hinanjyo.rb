b680332cd423a567df15428052c61660bce80b39
Derek Johnson <wars2cool@gmail.com>
2019-04-10 10:57:16 +0900

    Updated Seeds--added some likes to shelters and fixed Votable errors

--- app/models/hinanjyo.rb
+++ app/models/hinanjyo.rb
@@ -4,6 +4,7 @@ class Hinanjyo < ApplicationRecord
   validates :place_name, presence: true
   validates :street, presence: true
 
+  acts_as_votable
   geocoded_by :street
   after_validation :geocode, if: :will_save_change_to_street?
 end
