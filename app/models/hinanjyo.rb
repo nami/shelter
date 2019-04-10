ee3115dcb88a6336eb992227718907b940dcf719
Derek Johnson <wars2cool@gmail.com>
2019-04-10 11:47:10 +0900

    Seeds update with fix and additional fake users

--- app/models/hinanjyo.rb
+++ app/models/hinanjyo.rb
@@ -3,8 +3,8 @@ class Hinanjyo < ApplicationRecord
   validates :prefecture, presence: true
   validates :place_name, presence: true
   validates :street, presence: true
-  
+
   acts_as_votable
-  geocoded_by :street
-  after_validation :geocode, if: :will_save_change_to_street?
+  # geocoded_by :street
+  # after_validation :geocode, if: :will_save_change_to_street?
 end
