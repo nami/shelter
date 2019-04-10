b6b05c32bd394911f0f39998478ce14ec3a2e281
nami izarra <nami.izarra@gmail.com>
2019-04-10 17:32:45 +0900

    detect location from home page

--- app/controllers/hinanjyos_controller.rb
+++ app/controllers/hinanjyos_controller.rb
@@ -8,9 +8,20 @@ class HinanjyosController < ApplicationController
     @shelters = Hinanjyo.where.not(latitude: nil, longitude: nil)
 
     if params[:location].blank?
-       @markers = @shelters
+      @markers = policy_scope(Hinanjyo)
     else
       @markers = Hinanjyo.near(params[:location], 3)
+    end
+
+    if params[:eq_form].present?
+      if params[:eq_form][:disaster].present?
+        @disaster = params[:eq_form][:disaster]
+        @shelters = Hinanjyo.where("#{@disaster} = true")
+        @markers = @shelters
+      elsif params[:eq_form][:longitude].present? && params[:eq_form][:disaster].present?
+        @shelters = Hinanjyo.near([params[:eq_form][:longitude],params[:eq_form][:latitude]], 3)
+        @markers = @shelters
+      end
     end
 
     @markers = @markers.map do |marker|
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+
+window.onload = function() {
+  var startPos;
+  var geoSuccess = function(position) {
+    startPos = position;
+    document.querySelector("input#eq_form_longitude").value = startPos.coords.latitude;
+    document.querySelector("input#eq_form_latitude").value = startPos.coords.longitude;
+  };
+  navigator.geolocation.getCurrentPosition(geoSuccess);
+};
+
+
