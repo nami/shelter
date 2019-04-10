71e8fec43bfcded5b0ea39f195dd8366c6dc0f0a
nami izarra <nami.izarra@gmail.com>
2019-04-10 19:21:16 +0900

    completed controller logic and search for earthquake

--- app/controllers/hinanjyos_controller.rb
+++ app/controllers/hinanjyos_controller.rb
@@ -9,18 +9,27 @@ class HinanjyosController < ApplicationController
 
     if params[:location].blank?
       @markers = policy_scope(Hinanjyo)
+    elsif session[:disaster].present? && session[:location].present?
+      @markers = Hinanjyo.where("#{session[:disaster]} = true")
+      @markers = Hinanjyo.near(session[:location], 3)
+    elsif session[:disaster].present?
+      @markers = Hinanjyo.where("#{session[:disaster]} = true")
+      @markers = Hinanjyo.near(params[:location], 3)
     else
       @markers = Hinanjyo.near(params[:location], 3)
+      session[:location] = params[:location]
     end
 
     if params[:eq_form].present?
-      if params[:eq_form][:disaster].present?
+      if params[:eq_form][:disaster].present? && params[:eq_form][:longitude].present?
+        @shelters = Hinanjyo.near([params[:eq_form][:longitude],params[:eq_form][:latitude]], 3)
+        session[:disaster] = params[:eq_form][:disaster]
+        @markers = @shelters
+      elsif params[:eq_form][:disaster].present?
         @disaster = params[:eq_form][:disaster]
         @shelters = Hinanjyo.where("#{@disaster} = true")
         @markers = @shelters
-      elsif params[:eq_form][:longitude].present? && params[:eq_form][:disaster].present?
-        @shelters = Hinanjyo.near([params[:eq_form][:longitude],params[:eq_form][:latitude]], 3)
-        @markers = @shelters
+        session[:disaster] = params[:eq_form][:disaster]
       end
     end
 
@@ -35,13 +44,19 @@ class HinanjyosController < ApplicationController
 
   def show
     authorize @shelter
-
   end
 
   private
 
   def find_shelter
     @shelter = Hinanjyo.find(params[:id])
+  end
+
+  def disaster
+    if params[:disaster].present?
+      session[:disaster] = params[:disaster]
+    end
+    return session[:disaster]
   end
 
 end
