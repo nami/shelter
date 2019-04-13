19cf7dfd3f06ee6f6bef9a201dcf3c81027e7c15
nami izarra <nami.izarra@gmail.com>
2019-04-14 01:47:10 +0900

    fixing layout for mobile2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,3 @@
 import "bootstrap";
 
-window.onload = function() {
-  var startPos;
-  var geoSuccess = function(position) {
-    startPos = position;
-    document.querySelector("input#eq_form_longitude").value = startPos.coords.latitude;
-    document.querySelector("input#eq_form_latitude").value = startPos.coords.longitude;
-  };
-  navigator.geolocation.getCurrentPosition(geoSuccess);
-};
 
-// window.onload = function() {
-//   var startPos;
-//   var geoSuccess = function(position) {
-//     startPos = position;
-//     document.querySelector("input#tsu_form_longitude").value = startPos.coords.latitude;
-//     document.querySelector("input#tsu_form_latitude").value = startPos.coords.longitude;
-//   };
-//   navigator.geolocation.getCurrentPosition(geoSuccess);
-// };
