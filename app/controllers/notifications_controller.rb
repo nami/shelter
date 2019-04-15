b7972a502f0ac07739a633bb26ec89210dcf8523
Peter Hsieh <chunhung.hsieh@gmail.com>
2019-04-15 13:21:08 +0900

    updating notification style

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,8 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = policy_scope(Notification).reverse
+    @today = Date.today
+    @yesterday = @today - 1.days
+    @month = @notifications.group_by { |t| t.created_at.month }
   end
 end
