01dbb107006b6a2173b57c45e335a441bd6f9eb2
Peter Hsieh <chunhung.hsieh@gmail.com>
2019-04-15 14:52:15 +0900

    if statement finished(maybe)

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,6 +3,6 @@ class NotificationsController < ApplicationController
     @notifications = policy_scope(Notification).reverse
     @today = Date.today
     @yesterday = @today - 1.days
-    @month = @notifications.group_by { |t| t.created_at.month }
+    # @month = @notifications.group_by { |t| t.created_at.month }
   end
 end
