00c1b95453d3e4b9c831bb24a555e51e2b7e3485
Peter Hsieh <chunhung.hsieh@gmail.com>
2019-04-16 10:37:31 +0900

    delete comments

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,13 +3,6 @@ class NotificationsController < ApplicationController
     @notifications = policy_scope(Notification).reverse.group_by { |t| t.created_at.to_date }.map { |k, v| [k, v.first] }.to_h
     policy_scope(Notification).update_all(checked: true)
 
-    # @notifications.each do |notification|
-    #   notification_day = custom_format(notification.created_at.to_date)
-    #   unless @sorted_notifications.key?(notification_day)
-    #     @sorted_notifications[notification_day] = []
-    #   end
-    #   @sorted_notifications[notification_day] << notification
-    # end
     Time.zone = "Japan"
     @today = Date.today
     @yesterday = @today - 1.days
