de54ec4ddca7297767c6f843f1c861b803f55e8c
nami izarra <nami.izarra@gmail.com>
2019-04-17 23:35:37 +0900

    setting up japanese

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_locale
+
+  def set_locale
+    I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
+  end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
 
   def default_url_options
     { host: ENV["HOST"] || "localhost:3000" }
